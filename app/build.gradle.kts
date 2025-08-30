ea258182db1c83ab2d4566baea795f1f98cc5659
DHRUVxMISHRA <dhruvm03122005@gmail.com>
2025-08-30 19:17:39 +0530

    This is my 2 commit In this commit we setup our dependency and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
@@ -56,4 +60,50 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:5.0.0")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta03")
+
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    // this ia for hillt dependency injection
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.57.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+
+
+    // this is for coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+
+    // Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+
+
+    //this is for pager
+    implementation ("com.google.accompanist:accompanist-pager:0.36.0")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.36.0")
+
+// this is for payment getway
+    implementation ("com.razorpay:checkout:1.6.41")
+
+
+    //custem bottom nev bar
+    implementation ("com.canopas.compose-animated-navigationbar:bottombar:1.0.2")
+
+    //lottie for Place order dialog
+    implementation ("com.airbnb.android:lottie-compose:6.6.7")
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
+
+    //splashScreen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
