aa8c06b738c146eb85932d6600f968dbdae9c277
DHRUVxMISHRA <dhruvm03122005@gmail.com>
2025-09-06 00:11:33 +0530

    This is my 6th commit in this we have set the home screen components

--- app/src/main/java/com/example/foodapp_fullstack/presentation/components/HomeScreenCards.kt
+++ app/src/main/java/com/example/foodapp_fullstack/presentation/components/HomeScreenCards.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -21,12 +22,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.foodapp_fullstack.R
@@ -154,6 +161,8 @@ fun PageCount(pagerState : PagerState) {
 @Composable
 fun DetailCard() {
 
+    val restaurantName = remember{ mutableStateOf("Haldiram's") }
+    val rating = remember { mutableStateOf("4.2") }
     Card(
         modifier = Modifier.fillMaxWidth()
             .height(100.dp),
@@ -162,6 +171,60 @@ fun DetailCard() {
         ),
         shape = RoundedCornerShape(bottomStart = 22.dp, bottomEnd = 22.dp)
     ) {
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 8.dp, end = 16.dp),
+                Arrangement.SpaceBetween) {
+                Text(text = restaurantName.value,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp)
+
+                Card(modifier = Modifier
+                    .size(width = 45.dp, height = 22.dp),
+                    shape = RoundedCornerShape(6.dp),
+                    colors = CardDefaults.cardColors(colorResource(R.color.teal_700)),
+                    ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = rating.value,
+                            modifier = Modifier
+                                .padding(start = 4.dp),
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 12.sp,
+                            color = Color.White
+                            )
+                        Icon(
+                            painter = painterResource(R.drawable.star),
+                            modifier = Modifier
+                                .padding(start = 5.dp, top = 6.dp, bottom = 2.dp)
+                                .size(10.dp),
+                            tint = Color.White,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(start = 16.dp, 6.dp)
+                    .width(165.dp)
+                    .height(22.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(Color.Gray.copy(alpha = 0.2f))
+            ) {
+
+            }
+        }
 
         
     }
