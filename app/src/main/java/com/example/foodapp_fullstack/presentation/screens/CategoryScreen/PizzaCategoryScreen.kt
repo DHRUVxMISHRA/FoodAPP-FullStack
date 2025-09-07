28fd0697e085244d0aabc128363d0191af865a1f
DHRUVxMISHRA <dhruvm03122005@gmail.com>
2025-09-08 00:40:07 +0530

    This is my 9th commit in this I have made the category screens

--- app/src/main/java/com/example/foodapp_fullstack/presentation/screens/CategoryScreen/PizzaCategoryScreen.kt
+++ app/src/main/java/com/example/foodapp_fullstack/presentation/screens/CategoryScreen/PizzaCategoryScreen.kt
@@ -1,2 +1,9 @@
 package com.example.foodapp_fullstack.presentation.screens.CategoryScreen
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PizzaCategoryScreen(navController: NavController) {
+    ChineseCategoryScreen(navController = navController)
+}
